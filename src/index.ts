3dbf1046c7689b57ccf8cb9e8f0da01f2e63ab93
nathankr <natankrasney@gmail.com>
2025-02-17 10:25:59 +0200

    fix compile error

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import { sum } from "./utils";
 
+const PERIOD_SEC = 5;
+console.log("hello typescript");
+console.log(`sum(13,33) : ${sum(13, 33)}`);
 
-console.log('hello typescript');
-console.log(`sum(13,33) : ${sum(13,33)}`); 
+const getMsec = () => new Date().getTime();
+const startMs = getMsec();
+const func = () => console.log(` Elapsed : ${getMsec() - startMs} [Ms]`);
+
+setInterval(func, PERIOD_SEC * 1000);
